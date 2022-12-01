dc439af1c6858e23243efc0082953ba7c841ae06
Roland Lamidieu <roland.lamidieu@gmail.com>
2022-12-01 14:12:04 +0100

    Rework day 1 to a module

--- 2022/day_1/main.ex
+++ 2022/day_1/main.ex
@@ -1,18 +1,28 @@
-{_, file} = File.read("in.txt")
+defmodule Solution do
+  def process_file(filename) do
+    case File.read(filename) do
+      {:ok, body} -> process_body(body)
+      {:error, reason} -> reason
+    end
+  end
 
-f = fn
-  x ->
-    x
-    |> Enum.map(fn value -> Integer.parse(value) end)
+  def process_body(body) do
+    body
+    |> String.split("\n\n")
+    |> Enum.map(&Solution.process_elf/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  def process_elf(elf) do
+    elf
+    |> String.split("\n")
+    |> Enum.map(&Integer.parse/1)
     |> Enum.map(fn value -> elem(value, 0) end)
     |> Enum.sum()
+  end
 end
 
-file
-|> String.split("\n\n")
-|> Enum.map(fn elf -> String.split(elf, "\n") end)
-|> Enum.map(f)
-|> Enum.sort(:desc)
-|> Enum.take(3)
-|> Enum.sum()
-|> IO.inspect()
+Solution.process_file("in.txt")
+|> IO.puts()
