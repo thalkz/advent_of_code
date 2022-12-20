ca78dad86decc4029f6aa95674a8bee4a0e902fe
Roland Lamidieu <roland.lamidieu@gmail.com>
2022-12-20 23:43:58 +0100

    Solve day 19 part 2

--- 2022/day_19/2.py
+++ 2022/day_19/2.py
@@ -40,13 +40,13 @@ class Factory:
 
     def should_build(self, robot_id) -> bool:
         if robot_id == 0:
-                return self.robots[0] < max(self.blueprint.robot_costs[1][0], self.blueprint.robot_costs[2][0])
+            return self.robots[0] < max(self.blueprint.robot_costs[1][0], self.blueprint.robot_costs[2][0])
         elif robot_id == 1:
-                return self.robots[1] < self.blueprint.robot_costs[2][1] and self.resources[1] < self.blueprint.robot_costs[2][1]+1
+            return self.robots[1] < self.blueprint.robot_costs[2][1] and self.resources[1] < self.blueprint.robot_costs[2][1]+3
         elif robot_id == 2:
-                return self.robots[2] < self.blueprint.robot_costs[3][2] and self.resources[2] < self.blueprint.robot_costs[3][2]+1
+            return self.robots[2] < self.blueprint.robot_costs[3][2] and self.resources[2] < self.blueprint.robot_costs[3][2]+2
         elif robot_id == 3:
-                return True
+            return True
 
     def build(self, robot_id) -> bool:
         self.resources = self.resources - self.blueprint.robot_costs[robot_id]
@@ -72,41 +72,41 @@ class Factory:
 
 
 call_count = 0
-# visited = []
+
 
 def step(factory: Factory) -> int:
     global visited
     global call_count
     call_count += 1
 
-    if call_count % 1_000_000 == 0:
-        print("call_count", call_count)
-
-    # print(factory.time)
-    # if str(factory) in visited[factory.time]:
-    #     return 0
-    # visited[time][str(factory)] = True
-
     if factory.time_is_up():
         return factory.resources[3]
 
     result = [0]
 
-    for id in range(3, -1, -1):
-        if factory.can_build(id):
-            if factory.should_build(id):
-                a = factory.copy()
-                a.mine()
-                a.build(id)
-                result.append(step(a))
-
-    factory.mine()
-    result.append(step(factory))
+    if factory.can_build(3):
+        factory.mine()
+        factory.build(3)
+        result.append(step(factory))
+    else:
+        should_mine_more = False
+        for id in range(3):
+            if id <= factory.best_robot and factory.should_build(id):
+                if factory.can_build(id):
+                    a = factory.copy()
+                    a.mine()
+                    a.build(id)
+                    result.append(step(a))
+                else:
+                    should_mine_more = True
+        if should_mine_more:
+            factory.mine()
+            result.append(step(factory))
     return max(result)
 
 
 qualities = []
-for line in open("small.txt"):
+for line in open("in2.txt"):
     numbers = [int(k) for k in re.findall("[0-9]+", line)]
     blueprint = Blueprint(numbers[0],
                           np.array([numbers[1], 0, 0, 0]),
@@ -120,12 +120,9 @@ for line in open("small.txt"):
     time = 0
     factory = Factory(blueprint, robots, resources, time)
 
-    visited = []
-    for k in range(33):
-        visited.append({})
     geodes = step(factory)
     print("geodes", geodes)
     print("call_count", call_count)
-    break
+    qualities.append(geodes)
 
-print("total qualities", qualities)
+print("total qualities", qualities[0] * qualities[1] * qualities[2])
