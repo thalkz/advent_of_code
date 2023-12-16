2f5412865b34e8ef1b3f3f60981db60d93513571
Roland Lamidieu <roland.lamidieu@gmail.com>
2023-12-16 15:04:06 +0100

    Solve day 16

---
+++ 2023/day_16/part_2/main.swift
@@ -0,0 +1,135 @@
+print("Part 2")
+
+func parseInput() -> Grid {
+    var grid: Grid = []
+    while let line = readLine() {
+        var row: [Character] = []
+        for char in line {
+            row.append(char)
+        }
+        grid.append(row)
+    }
+    return grid
+}
+
+typealias Grid = [[Character]]
+
+enum Direction {
+    case up, down, right, left
+    
+    func isVertical() -> Bool {
+        return self == .up || self == .down
+    }
+    func isHorizontal() -> Bool {
+        return self == .right || self == .left
+    }
+    
+    func transform(_ char: Character) -> [Direction] {
+        switch char {
+        case ".",
+            "-" where self.isHorizontal(),
+            "|" where self.isVertical():
+            return [self]
+        case "-":
+            return [.left, .right]
+        case "|":
+            return [.up, .down]
+        case "/":
+            switch self {
+            case .up:
+                return [.right]
+            case .down:
+                return [.left]
+            case .right:
+                return [.up]
+            case .left:
+                return [.down]
+            }
+        default:
+            switch self {
+            case .up:
+                return [.left]
+            case .down:
+                return [.right]
+            case .right:
+                return [.down]
+            case .left:
+                return [.up]
+            }
+        }
+    }
+}
+
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+    let dir: Direction
+}
+
+func move(_ x: Int, _ y: Int, _ dir: Direction) -> Point {
+    switch dir {
+    case .up:
+        return Point(x: x, y: y-1, dir: dir)
+    case .down:
+        return Point(x: x, y: y+1, dir: dir)
+    case .left:
+        return Point(x: x-1,y: y, dir: dir)
+    case .right:
+        return Point(x: x+1, y:y, dir: dir)
+    }
+}
+
+let grid = parseInput()
+
+func propagate(_ light: Point, _ visited: inout Set<Point>) {
+    if light.x < 0 || light.x >= grid[0].count || light.y < 0 || light.y >= grid.count {
+        return
+    }
+    
+    if visited.contains(light) {
+        return
+    }
+    
+    visited.insert(light)
+    
+    let outputDirs = light.dir.transform(grid[light.y][light.x])
+    for dir in outputDirs {
+        propagate(move(light.x, light.y, dir), &visited)
+    }
+}
+
+func count(_ visited: Set<Point>) -> Int {
+    var unique: Set<Point> = []
+    for visit in visited {
+        unique.insert(Point(x: visit.x, y: visit.y, dir: .up))
+    }
+    return unique.count
+}
+
+var maxEnergy = 0
+
+for j in 0...(grid.count-1) {
+    var visited: Set<Point> = []
+    propagate(Point(x:0, y:j, dir:.right), &visited)
+    maxEnergy = max(count(visited), maxEnergy)
+}
+
+for j in 0...(grid.count-1) {
+    var visited: Set<Point> = []
+    propagate(Point(x:grid[0].count-1, y:j, dir:.left), &visited)
+    maxEnergy = max(count(visited), maxEnergy)
+}
+
+for i in 0...(grid[0].count-1) {
+    var visited: Set<Point> = []
+    propagate(Point(x:i, y:0, dir:.down), &visited)
+    maxEnergy = max(count(visited), maxEnergy)
+}
+
+for i in 0...(grid[0].count-1) {
+    var visited: Set<Point> = []
+    propagate(Point(x:i, y:grid.count-1, dir:.up), &visited)
+    maxEnergy = max(count(visited), maxEnergy)
+}
+
+print(maxEnergy)
