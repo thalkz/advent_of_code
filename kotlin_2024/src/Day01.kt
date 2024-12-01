eddbcd21d4a5c4b340d86d4428535d12d423c853
Roland Lamidieu <roland.lamidieu@gmail.com>
2024-12-01 11:02:57 +0100

    add notes on implementations

--- kotlin_2024/src/Day01.kt
+++ kotlin_2024/src/Day01.kt
@@ -4,29 +4,30 @@ import kotlin.math.absoluteValue
 
 fun main() {
     fun parseLists(input: List<String>): Pair<IntArray, IntArray> {
-        val left = IntArray(input.size)
-        val right = IntArray(input.size)
-        input.forEachIndexed { i, line ->
-            val (leftValue, rightValue) = line.split("   ").map { it.toInt() }
-            left[i] = leftValue
-            right[i] = rightValue
+        val left = IntArray(input.size) // int[]
+        val right = IntArray(input.size) // int[]
+        input.forEachIndexed { i, line -> // kotlin.collections.Iterable<T>::forEachIndexed
+            val (leftValue, rightValue) = line
+                .split("   ") // kotlin.text.CharSequence::split (return a java.util.ArrayList<String>)
+            left[i] = leftValue.toInt()
+            right[i] = rightValue.toInt()
         }
-        return left to right
+        return left to right // kotlin.Pair (serializable data class)
     }
 
     fun part1(input: List<String>): Int {
         val (left, right) = parseLists(input)
-        left.sort()
-        right.sort()
-        return left.zip(right)
+        left.sort() // java.util.DualPivotQuicksort::sort
+        right.sort() // java.util.DualPivotQuicksort::sort
+        return left.zip(right) // kotlin.collections.IntArray::zip (returns a java.util.ArrayList<Pair<Int, Int>>)
             .sumOf {
-                (it.first - it.second).absoluteValue
+                (it.first - it.second).absoluteValue // java.lang.Math::abs
             }
     }
 
     fun part2(input: List<String>): Int {
         val (left, right) = parseLists(input)
-        val groupedRight = right.groupingBy().eachCount()
+        val groupedRight = right.groupingBy().eachCount() // uses kotlin.collections.Grouping::foldTo
         return left.sumOf { it * (groupedRight[it] ?: 0) }
     }
 
@@ -39,8 +40,8 @@ fun main() {
 
 // Does not exist in the std lib for IntArray
 private fun IntArray.groupingBy(): Grouping<Int, Int> {
-    return object : Grouping<Int, Int> {
-        override fun sourceIterator(): Iterator<Int> = this@groupingBy.iterator()
+    return object : Grouping<Int, Int> { // kotlin.collections.Grouping<T, out K)
+        override fun sourceIterator(): Iterator<Int> = this@groupingBy.iterator() // kotlin.collections.Iterator<out T>
         override fun keyOf(element: Int): Int = element
     }
 }
