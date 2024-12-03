a7082592055eb34a74f131a213446ee60a7ebd0f
Roland Lamidieu <roland.lamidieu@gmail.com>
2024-12-03 08:49:38 +0100

    create a script to generate a day

---
+++ kotlin_2024/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DAY="03"
+
+echo "Generating Day$DAY"
+touch src/Day"$DAY".txt
+touch src/Day"$DAY"_test.txt
+TEMPLATE=$(cat src/DayXX.kt)
+echo "${TEMPLATE//XX/$DAY}" > src/Day"$DAY".kt
---
+++ kotlin_2024/src/Day03.kt
@@ -0,0 +1,24 @@
+package com.thalkz
+
+fun main() {
+    fun parseInput(input: List<String>): List<String> {
+        return input
+    }
+
+    fun part1(input: List<String>): Int {
+        val parsed = parseInput(input)
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        val parsed = parseInput(input)
+        return 0
+    }
+
+    val inputFile = "Day03"
+    verify("${inputFile}_test", ::part1, 0)
+//    verify("${inputFile}_test", ::part2, 0)
+
+//    solvePart1(inputFile, ::part1)
+//    solvePart2(inputFile, ::part2)
+}
