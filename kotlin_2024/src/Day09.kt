3ce0cd71e02b7753f416cb04e1d0dee25cb4dade
Roland Lamidieu <roland.lamidieu@gmail.com>
2024-12-12 01:09:11 +0100

    cleanup day 9 part 1

--- kotlin_2024/src/Day09.kt
+++ kotlin_2024/src/Day09.kt
@@ -1,52 +1,29 @@
 package com.thalkz
 
 private sealed interface Block {
-    val position: Int
-
-    data class File(
-        val fileId: Int,
-        override val position: Int
-    ): Block {
+    data class File(val fileId: Int): Block {
         override fun toString() = fileId.toString()
     }
 
-    data class FreeMemory(
-        override val position: Int
-    ): Block {
+    data object FreeMemory: Block {
         override fun toString() = "."
     }
 }
 
 private class DiskReader(
     private val encodedMemory: String,
-    private val readFromLeft: Boolean
+    range: IntProgression,
 ): Iterable<Block> {
-
-    val memorySize = encodedMemory.sumOf { it - '0' }
-
-    val range = if (readFromLeft) {
-        0..encodedMemory.lastIndex
-    } else {
-        encodedMemory.lastIndex downTo 0
-    }
-
-    fun Int.toPosition() = if (readFromLeft) {
-        this
-    } else {
-        memorySize - 1 - this
-    }
-
     private val reader = sequence {
         var fileStart = 0
         for (cursor in range) {
             val fileSize = encodedMemory[cursor] - '0'
             val isFile = cursor%2 == 0
             for (i in 0..<fileSize) {
-                val position = (fileStart + i).toPosition()
                 if (isFile) {
-                    yield(Block.File(cursor / 2, position))
+                    yield(Block.File(cursor / 2))
                 } else {
-                    yield(Block.FreeMemory(position))
+                    yield(Block.FreeMemory)
                 }
             }
             fileStart+=fileSize
@@ -57,16 +34,14 @@ private class DiskReader(
 }
 
 fun main() {
-    fun parseInput(lines: List<String>): String {
-        return lines[0]
-    }
-
     fun defragment(disk: String): Sequence<Block.File> = sequence {
-        val leftReader = DiskReader(disk, readFromLeft = true).iterator()
-        val rightReader = DiskReader(disk, readFromLeft = false).filterIsInstance<Block.File>().iterator()
+        val diskSize = disk.sumOf { it - '0' }
+        val leftReader = DiskReader(disk, 0..disk.lastIndex).iterator()
+        val rightReader = DiskReader(disk, disk.lastIndex downTo 0).filterIsInstance<Block.File>().iterator()
         var leftBlock = leftReader.next()
         var rightBlock = rightReader.next()
-        while (leftBlock.position <= rightBlock.position) {
+        var counter = 2
+        while (counter < diskSize) {
             when (leftBlock) {
                 is Block.File -> {
                     yield(leftBlock)
@@ -74,21 +49,22 @@ fun main() {
                 is Block.FreeMemory -> {
                     yield(rightBlock)
                     rightBlock = rightReader.next()
+                    counter++
                 }
             }
             leftBlock = leftReader.next()
+            counter++
         }
     }
 
     fun part1(lines: List<String>): ULong {
-        val disk = parseInput(lines)
-        return defragment(disk)
+        return defragment(lines[0])
             .mapIndexed { position, block -> (position * block.fileId).toULong() }
             .sum()
     }
 
     fun part2(lines: List<String>): Int {
-        val line = parseInput(lines)
+        val disk = lines[0]
         return 0
     }
 
